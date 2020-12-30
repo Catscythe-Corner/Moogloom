2ff29b407082d97555345922de375f2d0a71e8f8
EltrutLikes <10eltrut1@gmail.com>
2020-12-30 16:17:18 -0500

    don't smoke pot kids

--- src/main/java/com/catscythe/moogloom/client/ClientRegister.java
+++ src/main/java/com/catscythe/moogloom/client/ClientRegister.java
@@ -7,6 +7,7 @@ import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -15,10 +16,10 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 @Mod.EventBusSubscriber(modid = Moogloom.MOD_ID, bus = Bus.MOD)
 public class ClientRegister {
 	
+	@SubscribeEvent
 	public static void registerClient(final FMLClientSetupEvent event) {
-		
 		RenderTypeLookup.setRenderLayer(MoogloomBlocks.NIGHTSHADE.get(), RenderType.getCutout());
-		
+		RenderTypeLookup.setRenderLayer(MoogloomBlocks.POTTED_NIGHTSHADE.get(), RenderType.getCutout());
 	}
 	
 }
--- src/main/java/com/catscythe/moogloom/core/registry/MoogloomBlocks.java
+++ src/main/java/com/catscythe/moogloom/core/registry/MoogloomBlocks.java
@@ -6,6 +6,7 @@ import com.minecraftabnormals.abnormals_core.core.util.registry.BlockSubRegistry
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.FlowerPotBlock;
 import net.minecraft.item.ItemGroup;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
@@ -17,5 +18,6 @@ public class MoogloomBlocks {
 	public static final BlockSubRegistryHelper HELPER = Moogloom.REGISTRY_HELPER.getBlockSubHelper();
 	
 	public static final RegistryObject<Block> NIGHTSHADE = HELPER.createBlock("nightshade", () -> new AbnormalsFlowerBlock(MoogloomEffects.MIDNIGHT::get, 120, Block.Properties.from(Blocks.POPPY)), ItemGroup.DECORATIONS);
+	public static final RegistryObject<Block> POTTED_NIGHTSHADE = HELPER.createBlockNoItem("potted_nightshade", () -> new FlowerPotBlock(NIGHTSHADE.get(), Block.Properties.from(Blocks.FLOWER_POT)));
 	
 }
